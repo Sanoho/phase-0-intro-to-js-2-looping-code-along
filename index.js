983b784cfe494a3e2a2c204e2826df43dbc70b63
Timothy <sanohoang@gmail.com>
2023-02-06 21:51:14 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(firstArray) {
+    let array = [];
+    for (let i = 0; i < firstArray.length; i++) {
+        array.push(`Thank you, ${firstArray[i]}, for the wonderful surprise gift!`);
+
+    };
+    return array;
+};
+
+writeCards();
+
+function countDown() {
+let number = 10;
+while(number >= 0) {
+    console.log(number--);
+}
+}
+
